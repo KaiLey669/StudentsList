e81268a4b9671fb1140e611f3b386e137d6d82eb
Vladislav Slobodchikov <vslobodchikov@vvo.local>
2026-06-15 23:11:58 +1000

    Refactor code

--- main.py
+++ main.py
@@ -1,77 +1,76 @@
-def calculate_average(grades: list) -> float:
+def calculate_average(grades: list[int|float]) -> float:
     try:
         average_grade = round(sum(grades) / len(grades), 2)
         return average_grade
     except ZeroDivisionError:
-        return 0
+        return 0.0
+
 
 def get_status(average_grade: float) -> str:
-    global status
+    status = ["Успешный", "Отстающий"]
     return status[0] if average_grade >= 75 else status[1]
 
-def update_average_grade(students: list) -> None:
-    average_grades = []
-    global common_average_grade
-    for student in students:
-        if validate_student(student):
-            average_grade = calculate_average(student["grades"])
-            average_grades.append(average_grade)
 
-    common_average_grade = calculate_average(average_grades)
-    print(f"\nОбщий средний балл: {common_average_grade}")
+def update_average_grade(students: list[dict]) -> None:
+    average_grades = []
+    for student in students:
+        average_grade = calculate_average(student["grades"])
+        average_grades.append(average_grade)
+
+    print(f"\nОбщий средний балл: {calculate_average(average_grades)}")
     print("------------------")
 
-def print_students(students: list) -> None:
+
+def print_students(students: list[dict]) -> None:
     for student in students:
-        if validate_student(student):
-            average_grade = calculate_average(student["grades"])
-            print(f"Студент: {student["name"]}",
-                  f"Средний балл: {average_grade}",
-                  f"Статус: {get_status(average_grade)}",
-                  "------------------",
-                  sep="\n")
-        else:
-            print("Ошибка в данных.\n------------------")
+        average_grade = calculate_average(student["grades"])
+        print(f"Студент: {student["name"]}",
+              f"Средний балл: {average_grade}",
+              f"Статус: {get_status(average_grade)}",
+              "------------------",
+              sep="\n")
+
 
 def validate_name(name: str) -> bool:
-    return True if len(name) > 0 else False
+    return True if name and type(name) is str else False
 
-def validate_grades(grades: list) -> bool:
-    if not len(grades):
-        return True
+
+def validate_grades(grades: list[int]) -> bool:
+    if type(grades) is not list:
+        return False
 
     for grade in grades:
-        if type(grade) not in [int, float] or not (grade >= 0 or grade <= 100):
+        if type(grade) is not int or not (grade >= 0 and grade <= 100):
             return False
     return True
+
 
 def validate_student(student: dict) -> bool:
     return True if validate_name(student["name"]) and validate_grades(student["grades"]) else False
 
-def add_student(students: list, name: str, grades: list) -> None:
-    global common_average_grade
+
+def add_student(students: list[dict], name: str, grades: list[int]) -> None:
     student = {"name": name, "grades": grades}
     if validate_student(student):
         students.append(student)
         print(f"\nСтудент {name} добавлен.")
     else:
-        print("Введены некорректные данные при добавлении студента.")
+        print("\nВведены некорректные данные при добавлении студента.\n")
         return
 
     update_average_grade(students)
 
-def delete_worst_student(students: list) -> None:
+
+def delete_worst_student(students: list[dict]) -> None:
     if not students:
+        print("Студенты в списке отсутствуют.")
         return
 
-    min_average_grade = 101
     average_grades = []
     for student in students:
-        if validate_student(student):
-            average_grade = calculate_average(student["grades"])
-            average_grades.append(average_grade)
-            if average_grade < min_average_grade:
-                min_average_grade = average_grade
+        average_grade = calculate_average(student["grades"])
+        average_grades.append(average_grade)
+    min_average_grade = min(average_grades)
 
     for index in range(len(average_grades)):
         if average_grades[index] == min_average_grade:
@@ -87,13 +86,11 @@ students = [
     {"name": "Vladik", "grades": [50]},
     {"name": "Vlados", "grades": [20, 30, 40]}
 ]
-status = ["Успешный", "Отстающий"]
-threshold_grade = 75
-common_average_grade = 0
+
 update_average_grade(students)
 print_students(students)
 
-add_student(students, "Vladosik", [80, 100, 30])
+add_student(students, "Vladosik", [70, 80, 90])
 print_students(students)
 
 delete_worst_student(students)
